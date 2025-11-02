ef38a5e5c355f995e810d586ece68bae3d96dc3d
Your Name <alaa.khaled_a108@fci.kfs.edu.eg>
2025-11-03 01:04:28 +0200

    Fix errors

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,23 +1,21 @@
 import clsx from "clsx";
-import { ElementType, ReactNode, ComponentPropsWithoutRef } from "react";
+import { ElementType, ReactNode, ComponentPropsWithRef } from "react";
 
-type BoundedProps<T extends ElementType = "section"> = {
+type BoundedProps<T extends ElementType> = {
   as?: T;
   className?: string;
   children?: ReactNode;
-} & Omit<ComponentPropsWithoutRef<T>, "as" | "children" | "className">;
+} & ComponentPropsWithRef<T>;
 
 export const Bounded = <T extends ElementType = "section">({
-  as: Comp = "section" as T,
+  as,
   className,
   children,
   ...restProps
 }: BoundedProps<T>) => {
+  const Comp = as || "section";
   return (
-    <Comp
-      className={clsx("px-4 first:pt-10 md:px-6", className)}
-      {...restProps}
-    >
+    <Comp className={clsx("px-4 first:pt-10 md:px-6", className)} {...restProps}>
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
