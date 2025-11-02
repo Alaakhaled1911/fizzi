13425b9b7a8127ab180783a7afbad2267431e8bd
Your Name <alaa.khaled_a108@fci.kfs.edu.eg>
2025-11-03 00:21:52 +0200

    first commit

---
+++ src/hooks/useStore.ts
@@ -0,0 +1,12 @@
+
+import { create } from 'zustand'
+interface state {
+    ready:boolean;
+    isReady:() => void ;
+}
+
+export const useStore = create<state>((set) => ({
+  ready: false,
+  isReady: () => set({ready:true }),
+
+}))
